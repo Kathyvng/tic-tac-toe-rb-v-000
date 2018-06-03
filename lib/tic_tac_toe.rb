225c66902731340486e42b2b10fcb6805ff8d63e
Kathy Nguyen <kathyvng@gmail.com>
2018-06-03 02:13:05 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -120,3 +120,14 @@ def current_player(board)
  end
  end
  
+ def play(board)
+   until over?(board) == true
+     turn(board)
+   end
+
+   if won?(board)
+     puts "Congratulations #{winner(board)}!"
+   elsif draw?(board)
+     puts "Cats Game!"
+   end
+ end
